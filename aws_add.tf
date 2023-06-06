dd4b136d610e74cabc463c26f959d2836efbb321
TaeHyong Kim <kthstar27@gmail.com>
2023-06-06 19:07:30 +0900

    1.0

--- aws_add.tf
+++ aws_add.tf
@@ -2,17 +2,17 @@
 locals {
     AWS_REG1_EIPs = [
         {
-            NAME = "AWS_SEOUL_NAT1_EIP"
+            NAME = "AWS-SEOUL-NAT1-EIP"
             # INS_ID = module.AWS_REG1_VPC1_INS.ID[0]
             NIC_ID = module.AWS_REG1_VPC1_INS.NIC_ID[0]
         }
         ,{
-            NAME = "AWS_SEOUL_NAT2_EIP"
+            NAME = "AWS-SEOUL-NAT2-EIP"
             # INS_ID = module.AWS_REG1_VPC1_INS.ID[2]
             NIC_ID = module.AWS_REG1_VPC1_INS.NIC_ID[2]
         }
         ,{
-            NAME = "IDC_SEOUL_CGW_EIP"
+            NAME = "IDC-SEOUL-CGW-EIP"
             # INS_ID = module.AWS_REG1_VPC2_INS.ID[0]
             NIC_ID = module.AWS_REG1_VPC2_INS.NIC_ID[0]
         }
@@ -23,12 +23,12 @@ locals {
 locals {
     AWS_REG2_EIPs = [
         {
-            NAME = "AWS_SINGAPORE_NAT_EIP"
+            NAME = "AWS-SINGAPORE-NAT-EIP"
             # INS_ID = module.AWS_REG2_VPC1_INS.ID[0]
             NIC_ID = module.AWS_REG2_VPC1_INS.NIC_ID[0]
         }
         ,{
-            NAME =  "IDC_SINGAPORE_CGW_EIP"
+            NAME = "IDC-SINGAPORE-CGW-EIP"
             # INS_ID = module.AWS_REG2_VPC2_INS.ID[0]
             NIC_ID = module.AWS_REG2_VPC2_INS.NIC_ID[0]
         }
